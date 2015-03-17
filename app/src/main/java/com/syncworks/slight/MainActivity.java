7a33d7ecc30392d1aa6d919f26384b3470280e1d
vosami <vosami@gmail.com>
2015-03-17 21:42:14 +0900

    blelib, graphview, dragndroplist add

--- app/src/main/java/com/syncworks/slight/MainActivity.java
+++ app/src/main/java/com/syncworks/slight/MainActivity.java
@@ -1,19 +1,60 @@
 package com.syncworks.slight;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.syncworks.vosami.blelib.BluetoothLeService;
 
 
 public class MainActivity extends ActionBarActivity {
+	private final static String TAG = MainActivity.class.getSimpleName();
+
+	// 블루투스 서비스
+	private BluetoothLeService bluetoothLeService;
+	private BluetoothAdapter bluetoothAdapter;
+
+	// 블루투스 서비스 연결
+	private final ServiceConnection lecServiceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			bluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+			if (!bluetoothLeService.initialize()) {
+				Log.e(TAG, "Unable to initialize Bluetooth");
+			}
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			bluetoothLeService = null;
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// 블루투스 장치 검색
+		Intent lecServiceIntent = new Intent(MainActivity.this, BluetoothLeService.class);
+		bindService(lecServiceIntent, lecServiceConnection, BIND_AUTO_CREATE);
+		final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		bluetoothAdapter = mBluetoothManager.getAdapter();
+		if (bluetoothAdapter == null) {
+			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT).show();
+			return;
+		}
 	}
 
 
