2c8ab2ae753fdcab57fe651cbd160613e96c0fc8
vosami@gmail.com <polki123>
2015-07-08 00:01:54 +0900

    test

---
+++ app/src/main/java/com/syncworks/slight/dialog/DialogImageView.java
@@ -0,0 +1,80 @@
+package com.syncworks.slight.dialog;
+
+import android.app.ActionBar;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.syncworks.slight.R;
+
+/**
+ * Created by Kim on 2015-07-07.
+ */
+public class DialogImageView extends Dialog{
+
+    ImageView imgView;
+    Button btnClose;
+
+    private Drawable imgDrawable = null;
+
+    private OnCloseListener closeListener = null;
+
+
+    public DialogImageView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();
+        lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        lpWindow.dimAmount = 0.8f;
+        getWindow().setAttributes(lpWindow);
+        setContentView(R.layout.dialog_image_view);
+        this.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        imgView = (ImageView) findViewById(R.id.image_view);
+        btnClose = (Button) findViewById(R.id.btn_close);
+
+        if (imgDrawable != null) {
+            imgView.setImageDrawable(imgDrawable);
+            imgView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnClose();
+            }
+        });
+    }
+
+    public void setImgView(Drawable drawable) {
+        imgDrawable = drawable;
+    }
+
+
+    public interface OnCloseListener {
+        void onBtnClose();
+    }
+
+    public void setOnCloseListener(OnCloseListener listener) {
+        closeListener = listener;
+    }
+
+    private void onBtnClose() {
+        if (closeListener != null) {
+            closeListener.onBtnClose();
+        }
+    }
+}
--- app/src/main/java/com/syncworks/slight/fragment_easy/InstallFragment.java
+++ app/src/main/java/com/syncworks/slight/fragment_easy/InstallFragment.java
@@ -1,22 +1,30 @@
 package com.syncworks.slight.fragment_easy;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
 
 import com.syncworks.slight.R;
+import com.syncworks.slight.dialog.DialogImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +35,14 @@ import com.syncworks.slight.R;
  */
 public class InstallFragment extends Fragment {
 
+    // 이미지 리소스
+    private final static int IMAGE_SCM1 = R.drawable.desc_scm100_1_t;
+    private final static int IMAGE_SCM2 = R.drawable.desc_scm100_2_t;
+    private final static int IMAGE_SCM3 = R.drawable.desc_scm100_3_t;
+    private final static int IMAGE_SPM1 = R.drawable.desc_spm100_1_t;
+    private final static int IMAGE_SPM2 = R.drawable.desc_spm100_2_t;
+    private final static int IMAGE_SPM3 = R.drawable.desc_spm100_3_t;
+    private final static int IMAGE_SPM4 = R.drawable.desc_spm100_4_t;
 
     private ToggleButton tbScm, tbSpm;
     private ImageButton ibScm1, ibScm2, ibScm3;
@@ -121,18 +137,25 @@ public class InstallFragment extends Fragment {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.desc_btn_scm_1:
+                    showImage(IMAGE_SCM1);
                     break;
                 case R.id.desc_btn_scm_2:
+                    showImage(IMAGE_SCM2);
                     break;
                 case R.id.desc_btn_scm_3:
+                    showImage(IMAGE_SCM3);
                     break;
                 case R.id.desc_btn_spm_1:
+                    showImage(IMAGE_SPM1);
                     break;
                 case R.id.desc_btn_spm_2:
+                    showImage(IMAGE_SPM2);
                     break;
                 case R.id.desc_btn_spm_3:
+                    showImage(IMAGE_SPM3);
                     break;
                 case R.id.desc_btn_spm_4:
+                    showImage(IMAGE_SPM4);
                     break;
                 case R.id.youtube_btn_scm:
                     watchYoutubeVideo("WqSx4s1G2N8");
@@ -168,6 +191,8 @@ public class InstallFragment extends Fragment {
         mListener = null;
     }
 
+
+    // 유튜브 비디오 플레이
     public void watchYoutubeVideo(String id){
         try{
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
@@ -177,6 +202,51 @@ public class InstallFragment extends Fragment {
                     Uri.parse("http://www.youtube.com/watch?v="+id));
             startActivity(intent);
         }
+    }
+    public void showImage(int drawableID) {
+        final DialogImageView dialog = new DialogImageView(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setImgView(getResources().getDrawable(drawableID));
+        dialog.setOnCloseListener(new DialogImageView.OnCloseListener() {
+            @Override
+            public void onBtnClose() {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+        /*Dialog builder = new Dialog(getActivity());
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                //nothing;
+            }
+        });
+
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageDrawable(getResources().getDrawable(drawableID));
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();*/
+    }
+    private void showImage2(int drawableID)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Hello World");
+        builder.setIcon(drawableID);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
